0d04edcb0082da66fb5e2c607b62da38cca5c7b3
Susan Wu <susannmwu@gmail.com>
2023-06-12 20:43:40 -0700

    js objects

--- js-practice.js
+++ js-practice.js
@@ -39,3 +39,90 @@ for (const kitty in kitties) {
 
 console.log(kitties.slice(2))
 console.log(kitties.slice(2,3))
+
+//Arrow functions not subjected to variable hoisting
+const functionName = (listOfParams) => {
+
+};
+
+function subtract(x,y) {
+    return x -y;
+}
+
+const subtract = (x,y) => {
+    return x - y; 
+};
+
+function add(x,y=0) {
+    return x+y;
+}
+
+const add = (x, y=0) => {
+    return x+y;
+};
+
+
+//one line return statement, you can leave out the curly braces and return statement 
+
+const subtract = (x,y) => x-y;
+
+const add = (x,y) => x+y;
+
+//omit the paratheses if you only have one parameter 
+
+const printMsg = msg => {
+    console.log(msg);
+};
+
+// JavaScript Objects are a lot like Python dictionaries 
+// sometimes called plain objects 
+
+
+const melonCodes = { 
+    cren: "Crenshaw",
+    musk: "Muskmelon"
+};
+
+// attributes are called keys or properties 
+// for our purposes, all keys in a JS obj has to be a string
+
+
+// to get keys in JS object, I can get nums[1] or nums["1"] => "one"
+
+const cat = {
+    name: "Marvin",
+    "age": 2,
+    "favorite smell": "cotton",
+};
+
+// end of lecture practice 
+
+const capitals = {
+    CA: "Sacramento",
+    "MD": "Anapolis"
+};
+
+// capitals["CA"]; => "Sacramento"
+
+// captials[MD]; => Reference error
+// this is looking for a variable calle MD and doesn't find one
+// hidden strings can only be hidden inside of objects
+
+// capitals.MD; => "Anapolis"
+
+// const calif = "CA";
+// captials[calif] => "Sacramento"
+
+// capitals.calif; => Undefined 
+// capitals.calif is looking for capitals["calif"]
+
+// capitals["MN"] = "St. Paul";
+// capitals.OR = "Salem";
+// capitals; 
+
+// const capitals = {
+//     CA: "Sacramento",
+//     "MD": "Anapolis",
+//     "MN": "St. Paul",
+//     "OR": "Salem"
+// };
